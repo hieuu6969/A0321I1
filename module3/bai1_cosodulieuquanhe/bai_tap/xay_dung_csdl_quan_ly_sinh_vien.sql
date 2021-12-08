19eeda26799cf2eaab357fd34845cc844c4a627f
HIEU DINH <dvhieu.espd.ufl@gmail.com>
2021-12-08 19:21:57 +0700

    update

---
+++ module3/bai1_cosodulieuquanhe/bai_tap/xay_dung_csdl_quan_ly_sinh_vien.sql
@@ -0,0 +1,6 @@
+create database bai_tap_01;
+use bai_tap_01;
+create table class (id int primary key, class_name varchar(50));
+create table teacher (id int primary key, teacher_name varchar(50), age int,country varchar(15));
+select * from class;
+select * from teacher
