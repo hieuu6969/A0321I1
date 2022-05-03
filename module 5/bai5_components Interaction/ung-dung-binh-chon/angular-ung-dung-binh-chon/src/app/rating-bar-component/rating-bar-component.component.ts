b7e833ac2d04ad6d6ef1e8e3d6e4c732bae2e4dc
HIEU DINH <dvhieu.espd.ufl@gmail.com>
2022-05-03 22:39:07 +0700

    update

---
+++ module 5/bai5_components Interaction/ung-dung-binh-chon/angular-ung-dung-binh-chon/src/app/rating-bar-component/rating-bar-component.component.ts
@@ -0,0 +1,63 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IRatingUnit} from "../irating-unit";
+
+@Component({
+  selector: 'app-rating-bar-component',
+  templateUrl: './rating-bar-component.component.html',
+  styleUrls: ['./rating-bar-component.component.css']
+})
+export class RatingBarComponentComponent implements OnInit {
+  backgroundColor = "#E4E4E4";
+  ratingColor = "#FFCC0B"
+  @Input() ratingScore: number;
+  @Input() ratingUnit: IRatingUnit = {};
+  @Input() ratingUnits: IRatingUnit[] = [
+    {
+      value:1,
+      active: false
+    },{
+      value:2,
+      active: false
+    },{
+      value:3,
+      active: false
+    },{
+      value:4,
+      active: false
+    },{
+      value:5,
+      active: false
+    },{
+      value:6,
+      active: false
+    },{
+      value:7,
+      active: false
+    },{
+      value:8,
+      active: false
+    },{
+      value:9,
+      active: false
+    },{
+      value:10,
+      active: false
+    },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.changeRating(this.ratingUnits[this.ratingScore-1])
+  }
+
+  changeRating(ratingUnit:IRatingUnit) {
+    this.ratingScore = ratingUnit.value;
+    for (let rating of this.ratingUnits) {
+      if (rating.value <= ratingUnit.value ){
+        rating.active = true;
+      } else {
+        rating.active = false;
+      }
+    }
+  }
+}
